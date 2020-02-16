1f55e05de8568a6d6d0bff606ca6c02a910e7c99
forsonem <forsonem@gmail.com>
2020-02-16 16:32:42 -0600

    note create view complete

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,10 @@ namespace ElevenNote.WebMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            var userId = Guid.Parse(User.Identity.GetUserId()); //sets userId to current user
+            var service = new NoteService(userId); //access service layer using userId
+            var model = service.GetNotes(); //model is now based of the model info puled from the database beimg used in the GetNotes method
+
             return View(model);
         }
 
@@ -27,13 +32,28 @@ namespace ElevenNote.WebMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-
+                return View(model);
             }
+            var service = AccessNoteService();
+
+            if (service.CreateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Note could not be created.");
             return View(model);
 
+            
+        }
 
+        private NoteService AccessNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
         }
     }
 }
