93ccdb02d758fa6dc95e5ab90fa179caa48ad995
forsonem <forsonem@gmail.com>
2020-02-16 15:02:43 -0600

    note data layet setup

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Date Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Date Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+    }
+}
